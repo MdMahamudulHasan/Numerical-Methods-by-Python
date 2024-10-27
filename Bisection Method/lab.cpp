7acfd9ebd2d8d3de8b17825243eec8c6536ccf09
Md Mahamudul Hasan <126501007+MdMahamudulHasan@users.noreply.github.com>
2024-10-28 04:25:09 +0600

    Initial commit

--- Bisection Method/lab.cpp
+++ Bisection Method/lab.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 
 double f(double x) {
@@ -15,27 +16,40 @@ void bijection(double a, double b) {
         return;
     }
 
-    
+    int i = 0;
+    cout << fixed << setprecision(5);
     while (fabs(b - a) > Accuracy) {
-        c = (a + b) / 2; 
+        c = (a + b) / 2;
+
+        cout << "Step = " << i + 1
+            << "  a = " << a
+            << "   f(a) = " << f(a)
+            << "   b = " << b
+            << "   f(b) = " << f(b)
+            << "   c = " << c
+            << "   f(c) = " << f(c);
 
         if (fabs(f(c)) < Accuracy) {
-            cout << "Root found: " << c << endl;
+            cout << "\nRoot found: " << c << endl;
             return;
         }
 
-        if (f(c) * f(a) < 0) { 
+        if (f(c) * f(a) < 0) {
             b = c;
+            cout << "   update: b = c" << endl;
         } else {
             a = c;
+            cout << "   update: a = c" << endl;
         }
+
+        i++; // Increment step counter
     }
 
-    cout << "Root found: " << c << endl;
+    cout << "\nRoot found: " << c << endl;
 }
 
 int main() {
-    double a, b; // Use double for more accurate input
+    double a, b;
     cout << "Enter value of a: ";
     cin >> a;
 
